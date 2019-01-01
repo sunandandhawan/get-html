ca665b8828ff6c28cc8d1f44cba4c54831dce325
Sunandan Dhawan <sunandan94@gmail.com>
2019-01-01 19:32:06 +0530

    initialization

---
+++ app.js
@@ -0,0 +1,54 @@
+// Copyright 2018, Google LLC.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const express = require('express');
+var request = require('request');
+
+const app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// [START hello_world]
+// Say hello!
+app.get('/', (req, res) => {
+  request(req.query.url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.status(200).send(body);
+    }
+  });
+  //console.log(req.query);
+  //res.status(200).send('Hello');
+});
+
+app.get('/hello', (req, res) => {
+  res.status(200).send('Hello World!');
+});
+// [END hello_world]
+
+if (module === require.main) {
+  // [START server]
+  // Start the server
+  const server = app.listen(process.env.PORT || 8080, () => {
+    const port = server.address().port;
+    console.log(`App listening on port ${port}`);
+  });
+  // [END server]
+}
+
+module.exports = app;
